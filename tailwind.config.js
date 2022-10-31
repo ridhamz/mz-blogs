f2c1285a03a057360624fdd01c2dd5a0fa6a2f36
ridhamezrigui <ridhamezrigui07@gmail.com>
2022-10-31 15:17:52 +0100

    Add the yellow color as the primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.teal,
+        primary: colors.yellow,
         gray: colors.neutral,
       },
       typography: (theme) => ({
